8a8bc64b2100681ba0881b0c9fed6b43b86f05a0
Jorge Alexis Zanabria <63270774+ajmoose96@users.noreply.github.com>
2023-07-12 21:58:35 -0500

    Base apparel

---
+++ Newbie/base-apparel-coming-soon-master/script.js
@@ -0,0 +1,10 @@
+// const formulario = document.querySelector('form');
+// const correoInput = document.querySelector('#correo');
+// const mensajeError = document.querySelector('#mensaje-error');
+
+// formulario.addEventListener('submit', function(event) {
+//   if (!correoInput.validity.valid) {
+//     mensajeError.style.display = 'block';
+//     event.preventDefault();
+//   }
+// });
